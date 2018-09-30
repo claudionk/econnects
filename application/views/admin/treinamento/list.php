20fca1155aac4a864e4d19b2b253126dc2b92800
Cristiano Arruda <crisarruda.silva@gmail.com>
2018-09-30 12:29:10 -0300

    Visualização do treinamento

--- application/views/admin/treinamento/list.php
+++ application/views/admin/treinamento/list.php
@@ -1,10 +1,21 @@
+<style type="text/css">
+#treinamento {
+	position: fixed;
+	height: 100%;
+	width: 82%;
+}
+@media (max-width: 1024px) {
+	#treinamento {
+		width: 104%;
+	}
+}
+</style>
 <div class="layout-app" style="margin-left: -24px;"">
     <!-- row -->
     <div class="row row-app">
         <!-- col -->
         <div class="col-md-12">
-            <iframe src="https://novogestao.woli.com.br/pt-BR/Login/Index?returnUrl=%27/%27&amp;idUnidadeHierarquica=1&amp;redirecionaIndex=1&amp;login=23814358325" frameborder="0" 
-                style="position: fixed;height: 100%;width: 82%;"></iframe>
+            <iframe src="https://novogestao.woli.com.br/pt-BR/Login/Index?returnUrl='/'&idUnidadeHierarquica=1&redirecionaIndex=1&login=23814358325" frameborder="0" id="treinamento" ></iframe>
         </div>
     </div>
 </div>
