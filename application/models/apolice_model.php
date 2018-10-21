c6de7aeb6d098ed379d66f21123f265e8ff414b2
Cristiano Arruda <crisarruda.silva@gmail.com>
2018-10-20 23:10:15 -0300

    Retorno de slugs na API

--- application/models/apolice_model.php
+++ application/models/apolice_model.php
@@ -904,12 +904,14 @@ Class Apolice_Model extends MY_Model
       ->select("apolice_status.slug as apolice_status_slug")
       ->select("produto_parceiro_plano.produto_parceiro_id")
       ->select("produto_parceiro.parceiro_id as parceiro_seg_id")
+      ->select("parceiro_seg.slug as slug_parceiro_seg")
       ->select("parceiro.slug as slug_parceiro")
       ->select("produto_parceiro.slug_produto")
       ->join("apolice_status", "apolice.apolice_status_id = apolice_status.apolice_status_id", 'inner')
       ->join("produto_parceiro_plano", "apolice.produto_parceiro_plano_id = produto_parceiro_plano.produto_parceiro_plano_id", 'inner')
       ->join("produto_parceiro", "produto_parceiro_plano.produto_parceiro_id = produto_parceiro.produto_parceiro_id", 'inner')
-      ->join("parceiro", "parceiro.parceiro_id = produto_parceiro.parceiro_id", 'inner');
+      ->join("parceiro parceiro_seg", "parceiro_seg.parceiro_id = produto_parceiro.parceiro_id", 'inner')
+      ->join("parceiro", "parceiro.parceiro_id = apolice.parceiro_id", 'inner');
 
     if($pedido){
       $pedido = $pedido[0];
