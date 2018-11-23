30a817d48be609ce90311493cc61563050ab6707
Cristiano Arruda <crisarruda.silva@gmail.com>
2018-11-23 15:08:11 -0200

    Define ambiente
    padrao é homologação

--- index.php
+++ index.php
@@ -27,17 +27,18 @@ $host_servers['homologacao'] = array(
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-if(in_array($_SERVER['HTTP_HOST'], $host_servers['production']  )){
+// if(in_array($_SERVER['HTTP_HOST'], $host_servers['production']  )){
 
-    define('ENVIRONMENT', 'production');
-}elseif(in_array($_SERVER['HTTP_HOST'], $host_servers['homologacao']  )){
+//     define('ENVIRONMENT', 'production');
+// }elseif(in_array($_SERVER['HTTP_HOST'], $host_servers['homologacao']  )){
 
-    define('ENVIRONMENT', 'homologacao');
-}else {
+//     define('ENVIRONMENT', 'homologacao');
+// }else {
 
-    define('ENVIRONMENT', 'development');
-}
+//     define('ENVIRONMENT', 'development');
+// }
 
+define('ENVIRONMENT', 'homologacao');
 date_default_timezone_set('America/Sao_Paulo');
 
 /*
