33e76fe0ab0fd3d6bd557ee2289942e4aba1e5f9
Cristiano Arruda <crisarruda.silva@gmail.com>
2019-05-13 12:28:47 -0300

    Cenário CTA
    - melhoria para rodar o código sem concorrênica de banco de dados e aplicação

--- application/models/cta_movimentacao_model.php
+++ application/models/cta_movimentacao_model.php
@@ -12,14 +12,48 @@ Class Cta_Movimentacao_Model extends MY_Model
     //Configurações
     protected $return_type = 'array';
 
+    protected $time;
+
     public function __construct()
     {
         parent::__construct();
+        $this->time = -5; // minutes
     }
 
     public function run(){
 
-        $sql = " 
+        // cria um arquivo para evitar rodar 2x o mesmo script
+        $fileFull = "/var/log/httpd/cta_movimentacao.block";
+
+        if(file_exists($fileFull)){
+            // pega a data de hoje e a data da criacao do arquivo
+            $now = new DateTime(date('Y-m-d H:i:s'));
+            $fileModified = new DateTime(date ("Y-m-d H:i:s", filemtime($fileFull)));
+
+            // se já fazem 4h que foi criado deverá remover o arquivo
+            $diff = $now->diff($fileModified);
+            if ($diff->h >= 4){
+                unlink($fileFull);
+            } else {
+                return false;
+            }
+        }
+
+        error_log(date('d-m-Y H:i:s'), 3, $fileFull);
+
+        $this->integracao_realizada_parc();
+        $this->integracao_realizada_cli();
+        $this->novas_apolices();
+
+        // remove o arquivo
+        unlink($fileFull);
+
+    }
+
+
+    private function structure(){
+
+        $sqlBegin = " 
         replace cta_movimentacao( 
             pedido_id
             , apolice_id
@@ -86,95 +120,137 @@ Class Cta_Movimentacao_Model extends MY_Model
                     AND i.slug_group = 'ems-emissao'
                     AND d.chave = chave_emi
                 ) AS ultimo_envio_comissao
-            FROM (
-                ( 
-                    SELECT distinct
-                        c.cliente_id
-                        , concat(a.num_apolice, '|', LPAD(am.apolice_movimentacao_tipo_id, 2, '0')) as chave_emi
-                        , am.apolice_movimentacao_tipo_id
-                        , a.num_apolice
-                       , a.pedido_id
-                       , a.apolice_id
-                       , p.cotacao_id
-                    FROM 
-                        apolice a 
-                        JOIN pedido p on a.pedido_id = p.pedido_id
-                        JOIN cotacao c on p.cotacao_id = c.cotacao_id
-                        JOIN apolice_movimentacao am on a.apolice_id = am.apolice_id
-                        JOIN apolice_movimentacao_tipo amt on am.apolice_movimentacao_tipo_id = amt.apolice_movimentacao_tipo_id    
-                        , integracao i        
-                        , integracao_log il 
-                        , integracao_log_detalhe ild
-                    WHERE 
-                        a.deletado = 0 
-                        AND p.deletado = 0 
-                        AND c.deletado = 0
-                        and i.integracao_id = il.integracao_id
-                        and ild.integracao_log_id = il.integracao_log_id
-                        and i.slug_group in ('parc-emissao','ems-emissao')
-                        and concat(a.num_apolice, '|', LPAD(am.apolice_movimentacao_tipo_id, 2, '0')) = ild.chave
-                        and date_add( now(), interval -3 minute ) < IFNULL(ild.alteracao,ild.criacao)
-                )
-                union
-                ( 
-                    SELECT distinct
-                        c.cliente_id
-                        , concat(a.num_apolice, '|', LPAD(am.apolice_movimentacao_tipo_id, 2, '0')) as chave_emi
-                        , am.apolice_movimentacao_tipo_id
-                        , a.num_apolice
-                       , a.pedido_id
-                       , a.apolice_id
-                       , p.cotacao_id
-                    FROM 
-                        apolice a 
-                        JOIN pedido p on a.pedido_id = p.pedido_id
-                        JOIN cotacao c on p.cotacao_id = c.cotacao_id
-                        JOIN apolice_movimentacao am on a.apolice_id = am.apolice_id
-                        JOIN apolice_movimentacao_tipo amt on am.apolice_movimentacao_tipo_id = amt.apolice_movimentacao_tipo_id    
-                        , integracao i        
-                        , integracao_log il 
-                        , integracao_log_detalhe ild
-                    WHERE 
-                        a.deletado = 0 
-                        AND p.deletado = 0 
-                        AND c.deletado = 0
-                        and i.integracao_id = il.integracao_id
-                        and ild.integracao_log_id = il.integracao_log_id
-                        and i.slug_group = 'cliente'
-                        and c.cliente_id = ild.chave
-                        and date_add( now(), interval -3 minute ) < IFNULL(ild.alteracao,ild.criacao)
-                )
-                union
-                (
-                SELECT distinct
-                    c.cliente_id
-                    , concat(a.num_apolice, '|', LPAD(am.apolice_movimentacao_tipo_id, 2, '0')) as chave_emi
-                    , am.apolice_movimentacao_tipo_id
-                    , a.num_apolice
-                   , a.pedido_id
-                   , a.apolice_id
-                   , p.cotacao_id
-                FROM 
-                    apolice a 
-                    JOIN pedido p on a.pedido_id = p.pedido_id
-                    JOIN cotacao c on p.cotacao_id = c.cotacao_id
-                    JOIN apolice_movimentacao am on a.apolice_id = am.apolice_id
-                    JOIN apolice_movimentacao_tipo amt on am.apolice_movimentacao_tipo_id = amt.apolice_movimentacao_tipo_id    
-                WHERE 
-                    a.deletado = 0 
-                    AND p.deletado = 0 
-                    AND c.deletado = 0
-                    and date_add( now(), interval -3 minute ) < am.criacao
-                )
+            FROM ( ";
+
+        $sqlEnd = "
             ) AS x
         ) AS y ";
 
+        return [
+            'begin' => $sqlBegin, 
+            'end' => $sqlEnd
+        ];
+    }
+
+    public function integracao_realizada_parc(){
+        $q = $this->structure();
+
+        $sql = $q['begin'] . " 
+            SELECT distinct
+                c.cliente_id
+                , concat(a.num_apolice, '|', LPAD(am.apolice_movimentacao_tipo_id, 2, '0')) as chave_emi
+                , am.apolice_movimentacao_tipo_id
+                , a.num_apolice
+               , a.pedido_id
+               , a.apolice_id
+               , p.cotacao_id
+            FROM 
+                apolice a 
+                JOIN pedido p on a.pedido_id = p.pedido_id
+                JOIN cotacao c on p.cotacao_id = c.cotacao_id
+                JOIN apolice_movimentacao am on a.apolice_id = am.apolice_id
+                JOIN apolice_movimentacao_tipo amt on am.apolice_movimentacao_tipo_id = amt.apolice_movimentacao_tipo_id    
+                , integracao i        
+                , integracao_log il 
+                , integracao_log_detalhe ild
+            WHERE 
+                a.deletado = 0 
+                AND p.deletado = 0 
+                AND c.deletado = 0
+                and i.integracao_id = il.integracao_id
+                and ild.integracao_log_id = il.integracao_log_id
+                and i.slug_group in ('parc-emissao','ems-emissao')
+                and concat(a.num_apolice, '|', LPAD(am.apolice_movimentacao_tipo_id, 2, '0')) = ild.chave
+                and date_add( now(), interval {$this->time} minute ) < IFNULL(ild.alteracao,ild.criacao)
+        ". $q['end'];
+
         if ( $this->_database->query( $sql ) ){
-            echo "Success!";
+            echo "OK Parcelas\n";
+            return true;
         }
         else
         {
-            echo "Query failed!";
+            echo "Fail Parcelas\n";
+            return false;
+        }
+    }
+
+    public function integracao_realizada_cli(){
+        $q = $this->structure();
+
+        $sql = $q['begin'] . " 
+            SELECT distinct
+                c.cliente_id
+                , concat(a.num_apolice, '|', LPAD(am.apolice_movimentacao_tipo_id, 2, '0')) as chave_emi
+                , am.apolice_movimentacao_tipo_id
+                , a.num_apolice
+               , a.pedido_id
+               , a.apolice_id
+               , p.cotacao_id
+            FROM 
+                apolice a 
+                JOIN pedido p on a.pedido_id = p.pedido_id
+                JOIN cotacao c on p.cotacao_id = c.cotacao_id
+                JOIN apolice_movimentacao am on a.apolice_id = am.apolice_id
+                JOIN apolice_movimentacao_tipo amt on am.apolice_movimentacao_tipo_id = amt.apolice_movimentacao_tipo_id    
+                , integracao i        
+                , integracao_log il 
+                , integracao_log_detalhe ild
+            WHERE 
+                a.deletado = 0 
+                AND p.deletado = 0 
+                AND c.deletado = 0
+                and i.integracao_id = il.integracao_id
+                and ild.integracao_log_id = il.integracao_log_id
+                and i.slug_group = 'cliente'
+                and c.cliente_id = ild.chave
+                and date_add( now(), interval {$this->time} minute ) < IFNULL(ild.alteracao,ild.criacao)
+        ". $q['end'];
+
+        if ( $this->_database->query( $sql ) ){
+            echo "OK Clientes\n";
+            return true;
+        }
+        else
+        {
+            echo "Fail Clientes\n";
+            return false;
+        }
+    }
+
+    public function novas_apolices(){
+        $q = $this->structure();
+
+        $sql = $q['begin'] . " 
+            SELECT distinct
+                c.cliente_id
+                , concat(a.num_apolice, '|', LPAD(am.apolice_movimentacao_tipo_id, 2, '0')) as chave_emi
+                , am.apolice_movimentacao_tipo_id
+                , a.num_apolice
+               , a.pedido_id
+               , a.apolice_id
+               , p.cotacao_id
+            FROM 
+                apolice a 
+                JOIN pedido p on a.pedido_id = p.pedido_id
+                JOIN cotacao c on p.cotacao_id = c.cotacao_id
+                JOIN apolice_movimentacao am on a.apolice_id = am.apolice_id
+                JOIN apolice_movimentacao_tipo amt on am.apolice_movimentacao_tipo_id = amt.apolice_movimentacao_tipo_id    
+            WHERE 
+                a.deletado = 0 
+                AND p.deletado = 0 
+                AND c.deletado = 0
+                and date_add( now(), interval {$this->time} minute ) < am.criacao
+        ". $q['end'];
+
+        if ( $this->_database->query( $sql ) ){
+            echo "OK Novas Apólices\n";
+            return true;
+        }
+        else
+        {
+            echo "Fail Novas Apólices\n";
+            return false;
         }
     }
 
