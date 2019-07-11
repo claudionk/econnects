746f4f6c49d721dad56cb59d9b3f667859cbe515
Cristiano Arruda <crisarruda.silva@gmail.com>
2019-07-11 15:52:28 -0300

    Comissão
    - ajuste para gerar comissão para genericos

--- application/models/comissao_gerada_model.php
+++ application/models/comissao_gerada_model.php
@@ -103,7 +103,7 @@ Class Comissao_Gerada_Model extends MY_Model {
             LEFT JOIN  cotacao_equipamento ON cotacao_equipamento.cotacao_id = cotacao.cotacao_id AND cotacao_equipamento.deletado = 0
             LEFT JOIN  cotacao_seguro_viagem ON cotacao_seguro_viagem.cotacao_id = cotacao.cotacao_id AND cotacao_seguro_viagem.deletado = 0
             LEFT JOIN  cotacao_generico ON cotacao_generico.cotacao_id = cotacao.cotacao_id AND cotacao_generico.deletado = 0
-            INNER JOIN parceiro_relacionamento_produto ON cotacao.parceiro_id = parceiro_relacionamento_produto.parceiro_id AND parceiro_relacionamento_produto.produto_parceiro_id = cotacao_equipamento.produto_parceiro_id
+            INNER JOIN parceiro_relacionamento_produto ON cotacao.parceiro_id = parceiro_relacionamento_produto.parceiro_id AND parceiro_relacionamento_produto.produto_parceiro_id = ifnull(ifnull(cotacao_equipamento.produto_parceiro_id, cotacao_generico.produto_parceiro_id), cotacao_seguro_viagem.produto_parceiro_id)
             LEFT JOIN comissao_gerada ON pedido.pedido_id = comissao_gerada.pedido_id AND comissao_gerada.deletado = 0 
             INNER JOIN parceiro ON parceiro.parceiro_id = parceiro_relacionamento_produto.parceiro_id
             INNER JOIN parceiro_tipo ON parceiro.parceiro_tipo_id = parceiro_tipo.parceiro_tipo_id
