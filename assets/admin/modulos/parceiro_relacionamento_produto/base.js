e6f132cb9565a053754a5abe6a352ed097544edb
Cristiano Arruda <crisarruda.silva@gmail.com>
2019-08-11 21:07:28 -0300

    Cálculo do Seguro
    - validação de máximo de markup
    - validado cálculo NET com markup, considerando ou não o desconto da comissão do corretor

--- assets/admin/modulos/parceiro_relacionamento_produto/base.js
+++ assets/admin/modulos/parceiro_relacionamento_produto/base.js
@@ -1,9 +1,6 @@
-
 $(document).ready(function(){
 
-
     show_hide_campos();
-
 
     $('input[name=repasse_comissao]').change(function () {
         show_hide_campos();
@@ -12,14 +9,16 @@ $(document).ready(function(){
     $('input[name=desconto_habilitado]').change(function () {
         show_hide_campos();
     });
+
+    $('input[name=comissao_tipo]').change(function () { 
+        show_hide_campos(); 
+    });
+
     $("#pai_id").chained("#produto_parceiro_id");
 
 });
 
-
 function show_hide_campos(){
-
-
 
     if(($('input[name=repasse_comissao]:checked').val() == '1')){
         $(".repasse_habilitado").show();
@@ -27,22 +26,17 @@ function show_hide_campos(){
         $(".repasse_habilitado").hide();
     }
 
-
     if(($('input[name=desconto_habilitado]:checked').val() == '1')){
         $(".desconto_habilitado").show();
     }else {
         $(".desconto_habilitado").hide();
     }
 
+    if($('input[name=comissao_tipo]:checked').val() == '0'){ 
+        $(".comissao_tipo").show(); 
+    }else{ 
+        $(".comissao_tipo").hide(); 
+    } 
 
 }
-
-//
-
-
-
-
-
-
-
 
