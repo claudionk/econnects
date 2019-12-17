b790e8479b8a025965098870d7cfecefa567e6ea
Vanessa <vanessart@gmail.com>
2019-12-16 22:35:21 -0300

    Aumentar Botão Minha Conta e Comprar

--- application/views/admin/venda/equipamento/front/footer.php
+++ application/views/admin/venda/equipamento/front/footer.php
@@ -1,4 +1,4 @@
-<div class="btns-buyaccount">
+<div class="btns-buyaccount btns">
 	<div class="btn-whats">
 	    <a href="https://api.whatsapp.com/send?phone=55<?php echo $whatsapp;?>&text=<?php echo $whatsapp_msg;?>." title="Whatsapp">
 	        <i class="fa fa-whatsapp"></i>
