6c68d09d02621a51d71bf293a2a3034c3b718c81
Vanessa <vanessart@gmail.com>
2020-01-14 20:46:36 -0300

    Animação ao subir
    botao "mais produtos""
    centralizar botao cotaçao

--- application/views/admin/venda/equipamento/front/footer.php
+++ application/views/admin/venda/equipamento/front/footer.php
@@ -1,13 +1,16 @@
+
+<div class="maisprodutos">
+	<?php $this->load->view('admin/venda/equipamento/front/app/comprar'); ?>
+</div>
+
 <div class="btns-buyaccount btns">
 	<div class="btn-whats">
 	    <a href="https://api.whatsapp.com/send?phone=55<?php echo $whatsapp;?>&text=<?php echo $whatsapp_msg;?>." title="Whatsapp">
 	        <i class="fa fa-whatsapp"></i>
 	    </a>
 	</div>
-	<div class="btn-buy btn btn-app btn-primary btn-proximo border-primary background-primary">
-	    <a href="<?php echo base_url("$current_uri/comprar")?>" title="Mais Produtos">
-	        Mais Produtos
-	    </a>
+	 <div aria-controls="bs-navbar" aria-expanded="false" data-target="#bs-navbar" data-toggle="collapse" id="menu-comprar" class="btn-buy btn btn-app btn-primary btn-proximo border-primary background-primary">    
+		        Mais Produtos
 	</div>
 	<div class="btn-myaccount btn btn-app btn-primary btn-proximo border-primary background-primary">
 	    <a href="<?php echo base_url("$current_uri/login")?>" title="Minha Conta">
@@ -15,3 +18,10 @@
 	    </a>
 	</div>
 </div>
+
+<script>
+    $("#menu-comprar").click(function(e) {
+        e.preventDefault();
+        $(".maisprodutos").toggleClass("closed");
+    });
+</script>
