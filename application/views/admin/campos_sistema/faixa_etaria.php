36a41511a9fc9b18e678d3558c527b7d31e151aa
Vanessa <vanessart@gmail.com>
2020-02-02 01:13:17 -0300

    Ajustes Campos de Faixa Etaria

--- application/views/admin/campos_sistema/faixa_etaria.php
+++ application/views/admin/campos_sistema/faixa_etaria.php
@@ -4,23 +4,19 @@ if($_POST)
 //print_r($list);
 ?>
 
-        <label class=" control-label" for="<?php echo $field_name;?>"><?php echo $field_label ?></label>
-        <div class="col-md-12">
-            <?php
-                $count = 0;
-            foreach($list[$field_name] as $linha) { 
-                if ($count == 4){
-                 <div class="col-md-12">
-                }
-            ?>
-                <div class="col-md-<?php echo $tamanho ?>  form-group<?php echo (app_is_form_error($field_name)) ? ' has-error' : ''; ?>">
-                 De <?php echo (int)$linha['inicial']; ?> a <?php echo (int)$linha['final']; ?> anos
-                 <input class="form-control" type="text" <?php echo issetor($class) ?> id="qtd" name="qtd" type="text" value="<?php echo  isset($row[$field_name]) ? $row[$field_name] : set_value($field_name); ?>" /> 
-                 </div>     
-            <?php 
-            $count++;
-            }  ?>
-        </div>
-    <?php echo app_get_form_error($field_name); ?>
+<div style="display: inline-block;">
+    <div class="form-group">
+        <label class=" control-label inli" for="<?php echo $field_name;?>"><?php echo $field_label ?></label>
+    </div>
+    <div class="form-group">
+        <?php
+        foreach($list[$field_name] as $linha) { ?>
+            <div class="col-md-<?php echo $tamanho ?> form-group<?php echo (app_is_form_error($field_name)) ? ' has-error' : ''; ?>">
+                <label class=" control-label inli" for="qtd_<?php echo (int)$linha['inicial']; ?>" >De <?php echo (int)$linha['inicial']; ?> a <?php echo (int)$linha['final']; ?> anos</label>
+             <input class="form-control" type="text" <?php echo issetor($class) ?> id="qtd_<?php echo (int)$linha['inicial']; ?>" name="qtd_<?php echo (int)$linha['inicial']; ?>" type="text" value="<?php echo isset($linha['qtd']) ? (int)$linha['qtd'] : 0; ?>" /> 
+             </div>     
+        <?php }  ?>
     
-
+    <?php echo app_get_form_error($field_name); ?>
+    </div>
+</div>
