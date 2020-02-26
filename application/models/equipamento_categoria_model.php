726a44a903fd05a148a95808e0ec0f3aaa398260
Cristiano Arruda <crisarruda.silva@gmail.com>
2020-02-26 16:19:59 -0300

    Melhoria na Query para trazer todos os Tablets independente o nivel

--- application/models/equipamento_categoria_model.php
+++ application/models/equipamento_categoria_model.php
@@ -66,7 +66,7 @@ Class Equipamento_Categoria_Model extends MY_Model
         $this->_database->distinct();
         $this->_database->join("vw_Equipamentos e", "e.equipamento_sub_categoria_id = {$this->_table}.equipamento_categoria_id");
         $this->_database->where("e.deletado", 0);
-        $this->_database->where("{$this->_table}.equipamento_categoria_nivel > 1", NULL, FALSE);
+        // $this->_database->where("{$this->_table}.equipamento_categoria_nivel > 1", NULL, FALSE);
         if (!empty($categoria_pai_id)) $this->_database->where("e.equipamento_categoria_id", $categoria_pai_id);
         if (!empty($equipamento_marca_id)) $this->_database->where("e.equipamento_marca_id", $equipamento_marca_id);
         return $this;
