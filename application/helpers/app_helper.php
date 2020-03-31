637f6dda7678b1b550e66b30ce680ab30be03e9a
Claudio Nakayama <claudio.nm6@gmail.com>
2020-03-31 08:58:48 -0300

    Volta da versão anterior que ocasionou bug na geração dos registros com campo default 0 (zero), que preenchia com branco. Ex.: Campo NR_SEQ_MOV do Sinistro.

--- application/helpers/app_helper.php
+++ application/helpers/app_helper.php
@@ -1191,7 +1191,10 @@ function emptyor(&$var, $default = ' ') {
 if ( ! function_exists('isempty')) {
     function isempty(&$var, $default = ' ')
     {
-        return (!empty($var)) ? $var : $default; // $default : $var;
+        //return (!empty($var)) ? $var : $default; // $default : $var; 
+        /*TODO: Voltei a versão anterior para corrigir o campos com default 0 como por exemplo NR_SEQ_MOV do Sinistro. 
+        Só que esta funcionalidade tem que ser ajustada novamente para o caso da branch "Ajuste emissão Saúde via API"*/
+        return (strlen($var) > 0) ? $var : $default; // $default : $var;
     }
 }
 
