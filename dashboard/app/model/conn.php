1f92a3ae00df1ae3e6bd1b0304a9c86292beb5c8
Claudio Nakayama <claudio.nm6@gmail.com>
2020-05-27 08:46:20 -0300

    Primeiro Commit com as connections

---
+++ dashboard/app/model/conn.php
@@ -0,0 +1,23 @@
+<?php      
+    $conn = null;
+    if ($ambiente == 'PRODUCAO') {
+        try {
+            $conn = new PDO('mysql:host=191.243.196.9;dbname=sisconnects;charset=utf8', "", "", 
+                            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        }
+        catch (PDOException $e) {
+            print $e->getMessage();
+        } 
+    }elseif($ambiente == 'HOMOLOGACAO'){
+        try {
+            $conn = new PDO('mysql:host=191.243.196.35;dbname=sisconnects;charset=utf8', "root", "EAQdmh91181", 
+                            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        }
+        catch (PDOException $e) {
+            print $e->getMessage();
+        } 
+    }else{
+        print('<br>Não foi possível conectar no ambiente!');
+    }
+    //print('<br>Conectado em: ' . $ambiente);
+?>
