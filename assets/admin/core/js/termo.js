7bbc43af6c7dd389ef4a3fc6e4df391406748272
Vinícius Marsanati <vinicius.marsanati@gmail.com>
2020-09-22 20:24:23 -0300

    Alteração de senha no primeiro acesso

--- assets/admin/core/js/termo.js
+++ assets/admin/core/js/termo.js
@@ -10,3 +10,38 @@
         document.location.reload();
     })
 
+    $("#aceitar_termo").on("submit", function(e){
+        e.preventDefault();
+        var $self = $(this);
+        var $aceitarTermoError = $("#aceitar_termo-error");        
+        var confirmarSenha = $self.find('[name="confirmar_senha"]').val();
+
+        var data = {};
+        data.ajax       = 1;
+        data.senha_atual = $self.find('[name="senha_atual"]').val();
+        data.senha_nova  = $self.find('[name="senha_nova"]').val();
+
+        if(data.senha_nova != confirmarSenha){
+            $aceitarTermoError.html("Senhas incompativeis");
+            return false;
+        }
+
+        var url = this.action;
+        $.ajax({
+            url: url,
+            type: "POST",
+            dataType: "json",
+            data: data,
+            success: function(oResponse){
+                if(oResponse.status == true){
+                    location.reload();
+                }else{
+                    $aceitarTermoError.html(oResponse.message);
+                }
+            },
+            error: function(oResponse){
+                $aceitarTermoError.html(oResponse.responseText);
+            }
+        });
+    });
+
