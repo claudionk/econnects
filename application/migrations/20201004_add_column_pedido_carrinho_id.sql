7e318d2f36530116320526542b40bdedc6af7d92
Your Name <williammello86@gmail.com>
2020-10-04 01:44:29 -0300

    migration pedido_carrinho table e add column pedido_carrinho_id na tablea pedido

---
+++ application/migrations/20201004_add_column_pedido_carrinho_id.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `sisconnects`.`pedido` 
+ADD COLUMN `pedido_carrinho_id` INT NULL AFTER `alteracao`;
---
+++ application/migrations/20201004_create_pedido_carrinho.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `sisconnects`.`pedido_carrinho` (
+  `pedido_carrinho_id` INT NOT NULL AUTO_INCREMENT,
+  `created_at` DATETIME NULL,
+  `updated_at` DATETIME NULL,
+  `deleted_at` DATETIME NULL,
+  PRIMARY KEY (`pedido_carrinho_id`));
