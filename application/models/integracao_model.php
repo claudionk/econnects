443823d6cd53284fc51684aa76584f44bc18133f
Vinícius Marsanati <vinicius.marsanati@gmail.com>
2020-11-23 12:37:50 -0300

    Primeiro Commit

--- application/models/integracao_model.php
+++ application/models/integracao_model.php
@@ -340,12 +340,34 @@ Class Integracao_Model extends MY_Model
             }
 
             //Array com os arquivos que precisam ser integrados (ainda não foram integrados)
-            $aResultFile = $this->getFile($result, $file);
-            /*$aResultFile = array(
-                array(
-                    "file" => "/var/www/webroot/ROOT/econnects/assets/uploads/integracao/336/R/731B2WDIGITAL301020200076.TXT"
-                )
-            );*/
+
+            if (isset($_GET["fileName"])) {
+
+                $_fileName  = $_GET["fileName"];
+                $_tipo      = $_GET["tipo"];
+                $_filePath  = "/var/www/webroot/ROOT/econnects/assets/uploads/integracao/$integracao_id/$_tipo/$_fileName";
+
+                if (file_exists($_filePath)) {
+
+                    $aResultFile = array(
+                        array(
+                            "file" => $_filePath
+                        )
+                    );  
+
+                } else {
+
+                    $aResultFile = array();
+
+                }
+
+            } else {
+
+                $aResultFile = $this->getFile($result, $file);
+
+            }
+            
+            /**/
             foreach($aResultFile as $resultFile){
 
                 $result_process = [];
