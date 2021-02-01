7a4bd32edc7fd486d128fde8eaf9defa00e74cda
Your Name <williammello86@gmail.com>
2021-02-01 12:12:23 -0300

    loading e csv

--- assets/admin/template/js/core/demo/Demo.js
+++ assets/admin/template/js/core/demo/Demo.js
@@ -282,6 +282,9 @@
 }(this.materialadmin, jQuery)); // pass in (namespace, jQuery):
 
 function getReportBody(url) {
+
+	$('tbody').html('');
+	$('#loading_icon').show()
 	filters = document.querySelectorAll('[data-filter]');
 	data_filter = { 'data_filter': true };
 
@@ -293,12 +296,58 @@ function getReportBody(url) {
 		data: data_filter,
 		url: url,
 		success: function (result) {
+			$('#loading_icon').hide()
 			if (result == '<tr></tr>') {
 				result = "<tr><td colspan='99' style='color:red'>Não encontrado</tr>"
 			}
-			$('tbody').html('');
 			$('tbody').append(result)
+		},
+		fail: function () {
+			$('#loading_icon').hide()
 		}
 	});
+}
+
+function downloadCSV(csv, filename) {
+	var csvFile;
+	var downloadLink;
+
+	// CSV file
+	csvFile = new Blob([csv], { type: "text/csv" });
+
+	// Download link
+	downloadLink = document.createElement("a");
+
+	// File name
+	downloadLink.download = filename;
+
+	// Create a link to the file
+	downloadLink.href = window.URL.createObjectURL(csvFile);
+
+	// Hide download link
+	downloadLink.style.display = "none";
+
+	// Add the link to DOM
+	document.body.appendChild(downloadLink);
+
+	// Click download link
+	downloadLink.click();
+}
+
+function exportTableToCSV(filename) {
+	var csv = [];
+	var rows = document.querySelectorAll("table tr");
+
+	for (var i = 0; i < rows.length; i++) {
+		var row = [], cols = rows[i].querySelectorAll("td, th");
+
+		for (var j = 0; j < cols.length; j++)
+			row.push(cols[j].innerText);
+
+		csv.push(row.join(","));
+	}
+
+	// Download CSV file
+	downloadCSV(csv.join("\n"), filename);
 }
 
