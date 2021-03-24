70391b3ed5a89bb56d1dd3659468edae6e87cb39
dpereirasa <email>
2021-03-24 00:51:35 -0300

    adição de paginação na consulta de base API

--- application/models/pedido_model.php
+++ application/models/pedido_model.php
@@ -253,7 +253,6 @@ Class Pedido_Model extends MY_Model
                             break;
                         case "days_ago":
                             $this->_database->where('(select criacao from apolice_movimentacao am where am.apolice_id = apolice.apolice_id order by criacao desc limit 1) > DATE_SUB(CURDATE(),INTERVAL '.$value.' DAY)');
-                            //$this->_database->limit(3);
                             break;
                     }
                 }
